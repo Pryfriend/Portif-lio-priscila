0bebf895e7bdce14a41baedbef98c66204ae7a9f
Pryfriend <85323377+Pryfriend@users.noreply.github.com>
2024-05-02 18:21:23 -0400

    Add files via upload

---
+++ validate.js
@@ -0,0 +1,7 @@
+//Seu JavaScript de validação aqui
+//Em densenvolvimento
+var nome =  document.getElementById("Nome");
+var email = document.getElementById("mail");
+var assunto = document.getElementById("Assunto");
+
+
